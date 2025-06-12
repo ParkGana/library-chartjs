bd69ed41b7b4f8bf2705547b1d88978c38dd9b81
ParkGana <gnchoco97@gmail.com>
2025-06-12 18:43:25 +0900

    공통 레이아웃 수정

--- src/components/layout/PageLayout.tsx
+++ src/components/layout/PageLayout.tsx
@@ -2,7 +2,7 @@ import Breadcrumb from './Breadcrumb';
 
 const PageLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="flex flex-col gap-10 p-5">
+    <div className="flex flex-col gap-5 p-5">
       <Breadcrumb />
       <div className="w-full">
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5">{children}</div>
