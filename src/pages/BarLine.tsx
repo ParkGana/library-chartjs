da609965bd74dfaa45968d195d2b1fee0f0b48e4
ParkGana <gnchoco97@gmail.com>
2025-06-23 17:54:42 +0900

    페이지 접속 시 Realtime 데이터 초기화

--- src/pages/BarLine.tsx
+++ src/pages/BarLine.tsx
@@ -1,15 +1,49 @@
+import { useEffect, useState } from 'react';
 import BasicBarLine from '../components/chart/barline/Basic';
 import RangeBarLine from '../components/chart/barline/Range';
 import RealtimeSlideBarLine from '../components/chart/barline/RealtimeSlide';
 import RealtimeStackBarLine from '../components/chart/barline/RealtimeStack';
+import {
+  resetBarChartRealtimeSlideADataAPI,
+  resetBarChartRealtimeSlideBDataAPI,
+  resetBarChartRealtimeStackADataAPI,
+  resetBarChartRealtimeStackBDataAPI,
+  resetLineChartRealtimeSlideADataAPI,
+  resetLineChartRealtimeSlideBDataAPI,
+  resetLineChartRealtimeStackADataAPI,
+  resetLineChartRealtimeStackBDataAPI
+} from '../api/Reset';
 
 const BarLineChartPage = () => {
+  const [isReset, setIsReset] = useState<boolean>(false);
+
+  useEffect(() => {
+    const resetData = async () => {
+      await Promise.all([
+        resetBarChartRealtimeStackADataAPI(),
+        resetBarChartRealtimeStackBDataAPI(),
+        resetBarChartRealtimeSlideADataAPI(),
+        resetBarChartRealtimeSlideBDataAPI(),
+        resetLineChartRealtimeStackADataAPI(),
+        resetLineChartRealtimeStackBDataAPI(),
+        resetLineChartRealtimeSlideADataAPI(),
+        resetLineChartRealtimeSlideBDataAPI()
+      ]);
+      setIsReset(true);
+    };
+    resetData();
+  }, []);
+
   return (
     <>
-      <BasicBarLine />
-      <RealtimeStackBarLine />
-      <RealtimeSlideBarLine />
-      <RangeBarLine />
+      {isReset && (
+        <>
+          <BasicBarLine />
+          <RealtimeStackBarLine />
+          <RealtimeSlideBarLine />
+          <RangeBarLine />
+        </>
+      )}
     </>
   );
 };
